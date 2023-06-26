3ffdee495473c900ffe1258b0fb69128bc325d01
Skrye <16295244+Skrye@users.noreply.github.com>
2023-06-26 14:51:25 -0400

    complete lesson 3 step 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+function reverseString(string) {
+    let reversedString = "";
+    for (let i = 0; i < string.length; i++) {
+        reversedString += string[(string.length - i) - 1];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
