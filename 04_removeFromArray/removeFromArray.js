dccfe6f8f56a32c11b1f33234fc5a9a9a9b8a518
Skrye <16295244+Skrye@users.noreply.github.com>
2023-06-26 17:17:41 -0400

    complete lesson 4 step 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(array, ...elements) {
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i];
+        let index = array.indexOf(element);
+        if (index > -1) {
+          array.splice(index, 1);
+        } else if (index = -1) {
+          return array;
+        }
+    }
+    return array;
+}    
 
 // Do not edit below this line
 module.exports = removeFromArray;
