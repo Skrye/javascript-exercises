8bf75c09d9f35039b7cfefc1ed053ab8556886df
Skrye <16295244+Skrye@users.noreply.github.com>
2023-06-27 00:40:36 -0400

    complete lesson 5 test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+function sumAll(intOne, intTwo) {
+    let max = Math.max(intOne, intTwo);
+    let min = Math.min(intOne, intTwo);
+    let sumBetween = 0;
+    for (let i = min; i <= max; i++) {
+        sumBetween += i;
+    }
+    return sumBetween;
 };
 
 // Do not edit below this line
