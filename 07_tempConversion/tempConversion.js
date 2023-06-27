5c10e939730cd579f33fac6e032dcb72e283b365
Skrye <16295244+Skrye@users.noreply.github.com>
2023-06-27 03:39:27 -0400

    Complete lesson 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+// Celsius to Farenheit (0 Celsius x 9/5) + 32 = 32 Farenheit
+// Farenheit to Celsius (32 Farenheit - 32) x 5/9 = 0 Ceslius
+
+function convertToCelsius(temperature) {
+  let farenheit = temperature;
+  let celsius = ((temperature - 32) * 5/9);
+  if (!(Number.isInteger(celsius))) {
+    return Number(celsius.toFixed(1));
+  } else {
+    return celsius;
+  }
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(temperature) {
+  let celsius = temperature;
+  let farenheit = ((temperature * 9/5) + 32);
+  if (!(Number.isInteger(farenheit))) {
+    return Number(farenheit.toFixed(1));
+  } else {
+    return farenheit;
+  }
 };
 
 // Do not edit below this line
