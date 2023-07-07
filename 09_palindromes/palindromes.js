7c7407ec16c918f52b7fbb1d556fc8c9d01e4d19
Skrye <perin63501@gmail.com>
2023-07-07 19:38:01 -0400

    Complete excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    let originalString = string.toLowerCase().replaceAll(/[^a-z0-9]/g, "").split("").join("");
+    let reversedString = string.toLowerCase().replaceAll(/[^a-z0-9]/g, "").split("").reverse().join("");
+    if (originalString == reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
