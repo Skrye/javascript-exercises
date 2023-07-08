cbebfa4e2c85f21b2c3a55384e1e673246fafb84
Skrye <perin63501@gmail.com>
2023-07-08 01:12:17 -0400

    complete excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number <= 0) return "OOPS";    
+    const fibStart = [0, 1];
+    for (let i = 1; i < number; i++){
+        fibStart.push(fibStart[i] + fibStart[i - 1]);
+    }
+    return fibStart[number];
 };
 
 // Do not edit below this line
