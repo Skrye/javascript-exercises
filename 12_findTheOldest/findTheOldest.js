9c096a59c5836fe2ef67cc7a822c50412e3389c7
Skrye <16295244+Skrye@users.noreply.github.com>
2023-07-08 23:55:59 -0400

    Complete exercise 12 step 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+	let oldest = array.sort((a,b) => {
+		const last = a.yearOfDeath - a.yearOfBirth;
+		const next = b.yearOfDeath - b.yearOfBirth;
+		return last > next ? -1 : 1;
+	})
+	return oldest[0];
 };
 
 // Do not edit below this line
