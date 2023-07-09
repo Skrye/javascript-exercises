7d7537229a1e12dbf4ad632758b7f48ac3b65ead
Skrye <16295244+Skrye@users.noreply.github.com>
2023-07-09 02:37:21 -0400

    Complete Excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,18 @@
 const findTheOldest = function(array) {
-	let oldest = array.sort((a,b) => {
+	let date = new Date();
+	let morgue = JSON.parse(JSON.stringify(array));
+	for (let i = 0; i < array.length; i++) {
+		if (morgue[i].yearOfDeath == undefined) {
+			morgue[i].yearOfDeath = date.getFullYear();
+		}
+	}
+	let oldest = morgue.sort((a,b) => {
 		const last = a.yearOfDeath - a.yearOfBirth;
 		const next = b.yearOfDeath - b.yearOfBirth;
 		return last > next ? -1 : 1;
 	})
 	return oldest[0];
-};
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
